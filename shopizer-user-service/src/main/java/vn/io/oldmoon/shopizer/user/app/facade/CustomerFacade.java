0e0157cf269fd82c280ebbf4c7e1cb2839468bc6
Thai Doan <thaidoan868@gmail.com>
2026-02-13 17:25:51 +0700

    feat(token): add create and get feature

--- shopizer-user-service/src/main/java/vn/io/oldmoon/shopizer/user/app/facade/CustomerFacade.java
+++ shopizer-user-service/src/main/java/vn/io/oldmoon/shopizer/user/app/facade/CustomerFacade.java
@@ -7,9 +7,14 @@ import org.springframework.stereotype.Service;
 import vn.io.oldmoon.shopizer.user.app.dto.customer.CreatedUser;
 import vn.io.oldmoon.shopizer.user.app.dto.customer.PersistableCustomer;
 import vn.io.oldmoon.shopizer.user.app.populator.customer.CustomerPopulator;
+import vn.io.oldmoon.shopizer.user.bussiness.service.TokenService;
 import vn.io.oldmoon.shopizer.user.bussiness.service.keycloak.KeycloakService;
 import vn.io.oldmoon.shopizer.user.infra.model.CustomerProfile;
+import vn.io.oldmoon.shopizer.user.infra.model.Token;
 import vn.io.oldmoon.shopizer.user.infra.repository.CustomerProfileRepository;
+
+import java.time.Instant;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +22,7 @@ public class CustomerFacade {
     private final CustomerProfileRepository profileRepo;
     private final CustomerPopulator customerPopulator;
     private final KeycloakService keycloakService;
+    private final TokenService tokenService;
     // private final CustomerService customerService
     // private final TokenService tokenService
     // private final EmailService emailService
@@ -26,13 +32,24 @@ public class CustomerFacade {
         UserRepresentation userRep = customerPopulator.toUserRep(persistableCustomer);
         CustomerProfile profile = customerPopulator.toProfile(persistableCustomer);
 
+        // create a new user
         String userId = keycloakService.create(userRep);
         userRep.setId(userId);
         keycloakService.resetPassword(userId, persistableCustomer.getPassword());
 
-        // tokenService.generateCode()
-        // tokenService.create(userId, email, code, expiresAt, type=EMAIL_VERIFICATION)
+
+        // generate email verification token
+        Token token = Token.builder()
+                .email(userRep.getEmail())
+                .userId(UUID.fromString(userId))
+                .code(tokenService.generateCode())
+                .expiresAt(Instant.now().plusSeconds(5 * 60))
+                .build();
+        tokenService.create(token);
+
         // async
+        profile.setUserId(UUID.fromString(userId));
+
         //      customerService.create(profile)
         //      tokenService.create(token)
         //      emailService.sendEmailVerifyCode(customer, email, code)
--- shopizer-user-service/src/main/java/vn/io/oldmoon/shopizer/user/bussiness/service/TokenService.java
+++ shopizer-user-service/src/main/java/vn/io/oldmoon/shopizer/user/bussiness/service/TokenService.java
@@ -3,9 +3,38 @@ package vn.io.oldmoon.shopizer.user.bussiness.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import vn.io.oldmoon.shopizer.user.bussiness.exception.BusinessException;
+import vn.io.oldmoon.shopizer.user.bussiness.exception.ErrorCode;
+import vn.io.oldmoon.shopizer.user.infra.model.Token;
+import vn.io.oldmoon.shopizer.user.infra.repository.TokenRepository;
+
+import java.security.SecureRandom;
+import java.time.Instant;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class TokenService {
+    private final TokenRepository tokenRepo;
+
+    public String generateCode() {
+        SecureRandom random = new SecureRandom();
+        return String.format("%06d", random.nextInt(1_000_000));
+    }
+
+    @Transactional
+    public Token create(Token token) {
+        return tokenRepo.save(token);
+    }
+
+    public Token get(String email, String code) {
+        return tokenRepo.findFirstByEmailAndCodeAndExpiresAtAfter(email, code, Instant.now()).orElseThrow(() ->
+                new BusinessException(
+                        ErrorCode.NOT_FOUND,
+                        "Token with email '%s' and code '%s' not found'".formatted(email, code)
+                )
+        );
+    }
 }
