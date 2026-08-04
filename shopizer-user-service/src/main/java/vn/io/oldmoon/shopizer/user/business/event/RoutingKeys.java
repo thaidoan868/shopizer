3d2bb4145b0a87e1436b49fda81f375457dd5955
Thai Doan <thaidoan868@gmail.com>
2026-08-04 17:09:35 +0700

    chore: add keycloak-rabbitmq plugin to Keycloak. closes #16

---
+++ shopizer-user-service/src/main/java/vn/io/oldmoon/shopizer/user/business/event/RoutingKeys.java
@@ -0,0 +1,7 @@
+package vn.io.oldmoon.shopizer.user.business.event;
+
+public class RoutingKeys {
+  public static final String userCreatedByAdmin = "KK.EVENT.ADMIN.shopizer.SUCCESS.USER.CREATE";
+  public static final String userCreatedByClient =
+      "KK.EVENT.CLIENT.shopizer.SUCCESS.account-console.REGISTER";
+}
