7ed347210742d06bf23915590c8aa5f29f289507
Thai Doan <thaidoan868@gmail.com>
2026-08-12 09:36:09 +0700

    config: add AsyncConfig

---
+++ shopizer-common/src/main/java/vn/io/oldmoon/shopizer/common/web/config/AsyncConfig.java
@@ -0,0 +1,36 @@
+package vn.io.oldmoon.shopizer.common.web.config;
+
+import java.lang.reflect.Method;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+@Slf4j
+public class AsyncConfig implements AsyncConfigurer {
+
+  @Override
+  public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+    return new CustomAsyncExceptionHandler();
+  }
+
+  public static class CustomAsyncExceptionHandler implements AsyncUncaughtExceptionHandler {
+
+    @Override
+    public void handleUncaughtException(Throwable ex, Method method, Object... params) {
+      log.error(
+          "Async Exception in method: {} with message: {}", method.getName(), ex.getMessage(), ex);
+
+      for (Object param : params) {
+        log.error("Parameter value: {}", param);
+      }
+
+      // - Send notification to Slack / Telegram / Sentry
+      // - Save failed job details into a DB table for manual retry
+      // - Push metric to Prometheus/Grafana
+    }
+  }
+}
--- shopizer-user-service/src/main/java/vn/io/oldmoon/shopizer/user/business/event/listener/CustomerCreatedEventListener.java
+++ shopizer-user-service/src/main/java/vn/io/oldmoon/shopizer/user/business/event/listener/CustomerCreatedEventListener.java
@@ -2,10 +2,9 @@ package vn.io.oldmoon.shopizer.user.business.event.listener;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.event.TransactionPhase;
-import org.springframework.transaction.event.TransactionalEventListener;
-import vn.io.oldmoon.shopizer.common.event.EventListener;
 import vn.io.oldmoon.shopizer.user.business.event.CustomerCreatedEvent;
 import vn.io.oldmoon.shopizer.user.business.service.KeycloakService;
 import vn.io.oldmoon.shopizer.user.infra.data.constant.Role;
@@ -13,11 +12,11 @@ import vn.io.oldmoon.shopizer.user.infra.data.constant.Role;
 @Component
 @RequiredArgsConstructor
 @Slf4j
-public class CustomerCreatedEventListener implements EventListener<CustomerCreatedEvent> {
+public class CustomerCreatedEventListener {
   private final KeycloakService keycloakService;
 
-  @Override
-  @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+  @EventListener
+  @Async
   public void handle(CustomerCreatedEvent event) {
     log.info("Processing CustomerCreatedEvent keycloakUserId={}", event.keycloakUserId());
     keycloakService.assignRealmRole(event.keycloakUserId(), Role.CUSTOMER);
